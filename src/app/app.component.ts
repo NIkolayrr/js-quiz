3c102927aef21edca287a1e23d06a7f728f06de8
NIkolayrr <nikolay.r.rangelov@gmail.com>
2021-02-03 18:29:35 +0200

    styling fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { version } from 'package.json'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'js-quiz';
+export class AppComponent implements OnInit {
+  title = 'js-quiz'
+  version = version
+  ngOnInit() {}
 }
